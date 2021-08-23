b5e5b1fbaa69fc1dabcbf4724aebbfbffd7ea0da
TurboZ0 <87539784+TurboZ0@users.noreply.github.com>
2021-08-23 16:27:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-noseX="";
-noseY="";
+
 
 function preload() {
-    image47=loadImage ("https://i.postimg.cc/c1K8GY4d/Clown.png");
-    imageak=loadImage ("https://i.postimg.cc/bNfb0T1s/blueeyes-eyes-lens-freetoedit-11562869614sh2nmg4ah7-removebg-preview.png")
+
 }
 
 
@@ -19,13 +17,6 @@ function setup() {
 function foundresult() {
     console.log("Posenet has finally been loaded");
 }
-function poseing(onresult) {
-    console.log(onresult);
-    console.log(onresult[0].pose.nose.x);
-    console.log(onresult[0].pose.nose.y);
-    
-    noseX=onresult[0].pose.nose.x;
-    noseY=onresult[0].pose.nose.y;
 
     }
 
